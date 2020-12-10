ed4c7526bc8ccf1593d0d0c3a6e79772983f5ca2
rschpdr <pedro.resch@gmail.com>
2020-12-10 17:43:56 -0300

    fix env vars

--- src/apis/bookApi.js
+++ src/apis/bookApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:1234/api",
+  baseURL: process.env.REACT_APP_API_BASE,
 });
 
 const storedUser = localStorage.getItem("loggedInUser");
