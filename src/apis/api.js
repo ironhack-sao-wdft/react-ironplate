79ada7b3c7f3a6ea15c45075afeba2cb8616c513
rschpdr <pedro.resch@gmail.com>
2021-03-05 16:32:03 -0300

    minor fixes

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,20 +1,27 @@
 import axios from "axios";
 
-// Pré-configurando a URL padrão do servidor Axios aqui
+const apis = {
+  development: "A URL DO SEU SERVIDOR EXPRESS LOCAL AQUI",
+  production: "A URL DO SEU SERVIDOR DEPLOYADO NO HEROKU AQUI",
+};
+
+// Pré-configurando a URL padrão do nosso backend em uma instância do Axios
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE,
+  baseURL: apis[process.env.NODE_ENV],
 });
-
-// Verifica se já temos as informações do usuário logado no localStorage
-const storedUser = localStorage.getItem("loggedInUser");
-
-const loggedInUser = JSON.parse(storedUser || '""');
 
 // Configura a instância do Axios para injetar o cabeçalho de autenticação antes de cada requisição
 api.interceptors.request.use((config) => {
-  config.headers = {
-    Authorization: `Bearer ${loggedInUser.token}`,
-  };
+  // Verifica se já temos as informações do usuário logado no localStorage
+  const storedUser = localStorage.getItem("loggedInUser");
+
+  const loggedInUser = JSON.parse(storedUser || '""');
+
+  if (loggedInUser.token) {
+    config.headers = {
+      Authorization: `Bearer ${loggedInUser.token}`,
+    };
+  }
 
   return config;
 });
