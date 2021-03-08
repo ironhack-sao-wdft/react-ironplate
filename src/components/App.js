eebaea68eacef8a41ef8b98872bd390ed1415042
Felipe Freitas <afelipe.freitas@gmail.com>
2021-03-08 15:48:47 -0300

    [FRONT] Cart and Purchase pages ccreated

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Home from "../routeComponents/Home";
 import AuthRouter from "../routeComponents/auth/AuthRouter";
 
 import { AuthContextComponent } from "../contexts/authContext";
+import Cart from "../routeComponents/Cart";
+import Checkout from "../routeComponents/Checkout";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/auth" component={AuthRouter} />
+          <Route path='/cart' component={Cart} />
+          <Route path='/checkout' component={Checkout} />
         </Switch>
       </AuthContextComponent>
     </BrowserRouter>
