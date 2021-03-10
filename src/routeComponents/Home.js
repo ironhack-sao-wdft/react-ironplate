a1afa0740d750aebc0ab7dd084b02e10591e91a1
leonardbwkh <leonard_bwkh@hotmail.com>
2021-03-10 16:11:15 -0300

    fixed overlap in homepage

--- src/routeComponents/Home.js
+++ src/routeComponents/Home.js
@@ -9,7 +9,7 @@ function Home() {
   return (
     <div className="text-center home-bg-color">
       <div className="d-flex flex-wrap m-3 slogan-carousel-size ">
-        <div className="slogan-bg slogan-text d-flex flex-column flex-wrap justify-content-center align-items-center col-6">
+        <div className="slogan-bg slogan-text d-flex flex-column flex-wrap justify-content-center align-items-center col-12 col-md-6">
           <h3 className="text-size slogan-text d-flex flex-wrap">
             The most peculiar online store you've ever seen
           </h3>
@@ -18,16 +18,16 @@ function Home() {
             <br /> wished you had
           </h4>
         </div>
-        <div className="col-6 d-flex flex-column justify-content-center align-items-center">
+        <div className="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center mb-5">
           <AwesomeSlider style={{ height: "40vh" }}>
-            <div data-src="/images/logoWrittenOrange.png" className="m-5" />
+            <div data-src="/images/logoWrittenOrange.png" />
             <div data-src="/images/logoWrittenOrange.png" />
             <div data-src="/images/logoWrittenOrange.png" />
           </AwesomeSlider>
         </div>
       </div>
-      <div className="card-center mt-5 mb-5">
-        <ProductCard className="card-individual" />
+      <div className="card-center">
+        <ProductCard />
         <ProductCard />
         <ProductCard />
       </div>
