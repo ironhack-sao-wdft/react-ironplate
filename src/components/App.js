4757ffb0eee48e8c2e9a94d70399379ef7e6d4c9
filipefcintra <filipefcintra@gmail.com>
2021-07-28 04:31:04 +0100

    Done for today

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,12 @@ import AuthRouter from "../routeComponents/auth/AuthRouter";
 import CreatePostForum from "../routeComponents/forum/CreatePostForum";
 import AllPosts from "../routeComponents/forum/AllPosts";
 
+import CreatePostMoradia from "../routeComponents/habitation/CreatePostMoradia";
+import AllMoradias from "../routeComponents/habitation/AllMoradias";
+
+import CreatePostJobs from "../routeComponents/jobs/CreatePostJobs";
+import AllJobs from "../routeComponents/jobs/AllJobs";
+
 import { AuthContextComponent } from "../contexts/authContext";
 import Footer from "./Footer";
 import "../assets/styles/index.css";
@@ -16,10 +22,16 @@ function App() {
     <BrowserRouter>
       <AuthContextComponent>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/auth" component={AuthRouter} />
-          <Route path="/forum" component={AllPosts} />
-          <Route path="/criar-forum" component={CreatePostForum} />
+          <div className="container-rotas">
+            <Route exact path="/" component={Home} />
+            <Route path="/auth" component={AuthRouter} />
+            <Route path="/forum" component={AllPosts} />
+            <Route path="/criar-forum" component={CreatePostForum} />
+            <Route path="/criar-moradia" component={CreatePostMoradia} />
+            <Route path="/moradia" component={AllMoradias} />
+            <Route path="/criar-emprego" component={CreatePostJobs} />
+            <Route path="/emprego" component={AllJobs} />
+          </div>
         </Switch>
         <Footer />
       </AuthContextComponent>
---
+++ src/routeComponents/habitation/CreatePostMoradia.js
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import api from "../../apis/api";
+
+import TextInput from "../../components/TextInput";
+import SelectInput from "../../components/SelectInput";
+import axios from "axios";
+
+function CreatePostMoradia() {
+  const [state, setState] = useState({
+    title: "",
+    website: "",
+    description: "",
+    phone: "",
+    photo: "",
+    companyEmail: "",
+    type: "",
+    price: "",
+    room: "",
+  });
+
+  function handleChange(event) {
+    setState({ ...state, [event.target.name]: event.target.value });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await api.post("/moradia", {
+        ...state,
+      });
+      setState({
+        title: "",
+        website: "",
+        description: "",
+        phone: "",
+        photo: "",
+        companyEmail: "",
+        type: "",
+        price: "",
+        room: "",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return (
+    <div className="container mt-5">
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          type="text"
+          label="Título:"
+          name="title"
+          onChange={handleChange}
+          value={state.title}
+        />
+
+        <TextInput
+          type="text"
+          label="Website:"
+          name="title"
+          onChange={handleChange}
+          value={state.website}
+        />
+
+        <SelectInput
+          label="Tipo: "
+          name="title"
+          onChange={handleChange}
+          value={state.type}
+          items={["Apartamento", "Casa", "Quarto", "Loja", "Outro"]}
+        />
+
+        <SelectInput
+          label="Quantidade de quartos: "
+          name="room"
+          onChange={handleChange}
+          value={state.room}
+          items={["Estúdio", "1", "2", "3", "4+"]}
+        />
+
+        <TextInput
+          type="text"
+          label="Preço:"
+          name="title"
+          onChange={handleChange}
+          value={state.price}
+        />
+
+        <TextInput
+          type="text"
+          label="Url Imagem:"
+          name="Fotos"
+          onChange={handleChange}
+          value={state.photo}
+        />
+
+        <TextInput
+          type="text"
+          label="Telefone:"
+          name="phone"
+          onChange={handleChange}
+          value={state.phone}
+        />
+        <TextInput
+          type="text"
+          label="E-mail:"
+          name="companyEmail"
+          onChange={handleChange}
+          value={state.companyEmail}
+        />
+
+        <div>
+          <label>Descrição</label>
+          <textarea
+            className="form-control"
+            aria-label="Descrição"
+            name="description"
+            onChange={handleChange}
+            value={state.description}
+          ></textarea>
+        </div>
+
+        <div className="form-group">
+          <button className="btn btn-primary mt-3" type="submit">
+            Enviar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default CreatePostMoradia;
