938475e739a56ea6acf6a019d37c78667d413316
Caio Nogueira <nogueira998@gmail.com>
2021-10-04 14:12:55 +0200

    pull

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,41 @@
-//Precisa criar esse componente
+import { useContext } from "react";
+import { AuthContext } from "../contexts/authContext";
+import Home from "../routeComponents/Home";
+// import ImageUpload from "./ImageUpload";
 
+function Post() {
+  const { loggedInUser } = useContext(AuthContext);
 
-function Post(){
-
-
-return (
-    <div>Render do cartão do post</div>
-)
-
+  return (
+    <>
+      {!loggedInUser.user._id ? (
+        <Home />
+      ) : (
+        {/* <div>
+          <div>
+            <img src="" alt="profile" />
+            <h3>Fulano</h3>
+          </div>
+          <img src="" alt="post" />
+          <h4>
+            Fulano<span>Texto do post</span>
+          </h4>
+          <div>
+            Map
+            <p>
+              <b>Siclano</b>comentário
+            </p>
+          </div>
+          <form>
+            <input type="text" placeholder="Add a comment..." />
+            <button type="submit">Post</button>
+          </form>
+          <ImageUpload/>
+        </div> */}
+        
+      )}
+    </>
+  );
 }
 
-export default Post
+export default Post;
