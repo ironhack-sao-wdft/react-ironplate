adeee7a05cc16962ff15137ce8ef2acd7e24c078
William Galvao <wsg.online@gmail.com>
2021-12-11 15:30:44 -0300

    working on carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -31,22 +31,22 @@ export default function Carousel() {
       </div>
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src={backgroundcard} className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
+          <img src={backgroundcard} className="w-100" alt="..." />
+          <div className="carousel-caption">
             <h5>First slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
           </div>
         </div>
         <div className="carousel-item">
-          <img src={backgroundcard} className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
+          <img src={backgroundcard} className="w-100" alt="..." />
+          <div className="carousel-caption">
             <h5>Second slide label</h5>
             <p>Some representative placeholder content for the second slide.</p>
           </div>
         </div>
         <div className="carousel-item">
-          <img src={backgroundcard} className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block">
+          <img src={backgroundcard} className="w-100" alt="..." />
+          <div className="carousel-caption">
             <h5>Third slide label</h5>
             <p>Some representative placeholder content for the third slide.</p>
           </div>
