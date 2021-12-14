1f1ba92b515ac5fbc9cb4395eaa41659fdc13b3a
William Galvao <wsg.online@gmail.com>
2021-12-14 09:38:18 -0300

    navbar margin adjusts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
   const [invisibleClose, setInvisibleClose] = useState("");
 
   return (
-    <div className="my-5 mx-2 d-flex justify-content-between">
+    <div className="m-4 d-flex justify-content-between">
       <div className={`${invisibleAccount}`}>
         {/* <img src={AccountCircle} alt="User account" /> */}
         <AccountCircleIcon sx={{ color: "white" }} fontSize="large" />
