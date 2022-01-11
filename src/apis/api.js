b1184d1257d9dde613f8604b4eb6b5509a21381c
jfilinho <47835154+jfilinho@users.noreply.github.com>
2022-01-10 23:49:07 -0300

    commit deploy front

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apis = {
   development: "http://localhost:4000/api",
-  production: "https://cadastro-livro-api.herokuapp.com/api",
+  production: "https://cadastro-livro-api.herokuapp.com/api/v1",
 };
 
 // Pré-configurando a URL padrão do nosso backend em uma instância do Axios
