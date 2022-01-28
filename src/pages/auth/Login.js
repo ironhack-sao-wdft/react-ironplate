5b184473e2fa576833f7932a56187be54130bc1b
Victoria Miranda <vitorianevesmiranda@gmail.com>
2022-01-28 17:51:54 -0300

    data

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -38,7 +38,7 @@ function Login(props) {
       navigate("/");
     } catch (err) {
       console.error(err.response);
-      setErrors({ ...err.response.data.errors});
+      setErrors({ ...err.response});
     }
   }
 
