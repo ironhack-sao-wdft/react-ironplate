4e56a99da691e2fab450cd7b10cc96c61f8c8f50
Victoria Miranda <vitorianevesmiranda@gmail.com>
2022-02-07 20:48:55 -0300

    teste

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apis = {
   development: "http://localhost:4000/api",
-  production: "https://bookregister-api.herokuapp.com/api",
+  production: "https://booksregister.herokuapp.com",
 };
 
 // Pré-configurando a URL padrão do nosso backend em uma instância do Axios
