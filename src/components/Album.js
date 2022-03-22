66851609194f5749b5f00bd8dc632f82d7d539b7
jfilinho <filinhosouza10@gmail.com>
2022-03-22 10:33:00 -0300

    estilos da home

--- src/components/Album.js
+++ src/components/Album.js
@@ -134,19 +134,6 @@ function Album() {
               </div>
             </div>
           </div>
-
-          <a
-            href="https://wa.me/5511974751884?text=Olá,%20gostaria%20de%20mais%20
-              informações%20sobre%20hospedagem."
-            target="blank"
-            className="icons-contact"
-          >
-            <img
-              src="./img/whatsapp.png"
-              alt="icone WhatsApp"
-              className="whatsapp-link"
-            />
-          </a>
         </div>
       </div>
     </div>
