8a3cbb2c4af0fff9ebe17c8c16c0d18fcbd4dd6d
jfilinho <filinhosouza10@gmail.com>
2022-03-23 14:27:25 -0300

    livros

--- src/pages/Lista.js
+++ src/pages/Lista.js
@@ -19,20 +19,16 @@ function ListaLivro() {
   }, []);
 
   return (
-   <div className="album py-sm-0 bg-light">
-        <div className="container-fluid mb-2">
-          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            <div className="col">
-              
+         <div className="container-fluid mb-2">
+          <div className="row row-cols-1 row-cols-sm-2 row-col-md-4">
+               
         
         {listaLivro.map((currentLivroObj) => (
           <CardLivro key={currentLivroObj._id} {...currentLivroObj} />
         ))}
       </div>
     </div>
-    </div>
-    </div>
-    
+      
   
   );
 }
