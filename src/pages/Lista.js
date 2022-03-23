f78b44144546f107786a8fca4b516837a5437f37
jfilinho <filinhosouza10@gmail.com>
2022-03-23 13:26:21 -0300

    comit lista de livros

--- src/pages/Lista.js
+++ src/pages/Lista.js
@@ -19,13 +19,19 @@ function ListaLivro() {
   }, []);
 
   return (
-    <div className="container  d-flex justify-content-between">
-      <div className=" row-col-md-4">
+   <div className="album py-sm-0 bg-light">
+        <div className="container-fluid mb-2">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div className="col">
+              <div className="card shadow-sm photo-album">
         
         {listaLivro.map((currentLivroObj) => (
           <CardLivro key={currentLivroObj._id} {...currentLivroObj} />
         ))}
       </div>
+    </div>
+    </div>
+    </div>
     </div>
   
   );
