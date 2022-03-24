dd7a8b598cb80573df01bd7deb6a54b7f6a18193
jfilinho <filinhosouza10@gmail.com>
2022-03-24 13:14:32 -0300

    edit livro and card livro

--- src/pages/Livros/CardLivro.js
+++ src/pages/Livros/CardLivro.js
@@ -10,7 +10,7 @@ import ConfirmationModal from "../../components/ConfirmationModal";
 function CardLivro(props) {
   const [showModal, setShowModal] = useState(false);
 
-   useParams();
+   const { id } = useParams();
   const navigate = useNavigate();
 
   return (
--- src/pages/Livros/EditLivro.js
+++ src/pages/Livros/EditLivro.js
@@ -37,7 +37,7 @@ function EditLivro(props) {
       }
     }
     user();
-  }, [id, userData]);
+  }, [id]);
 
   function handleChange(e) {
     if (e.target.files) {
