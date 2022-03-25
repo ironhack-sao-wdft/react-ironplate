81c6bbcae7de20593cb58d3c378981cfc0e604ce
jfilinho <filinhosouza10@gmail.com>
2022-03-25 17:40:08 -0300

    novo commit form cadastro

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,7 @@ function App() {
     <div className="pagina-principal">
       <AuthContextComponent>
            <Navbar />
-          <div className="container-fluid mb-3 conteudo">
+          <div className="container-fluid mb-3">
             <Routes>
               <Route
                 path="/lista"
