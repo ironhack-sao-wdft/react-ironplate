b5e34597d8937a9652c6b0534a740ee8a2ac2bdc
jfilinho <filinhosouza10@gmail.com>
2022-03-25 13:56:21 -0300

    tamanhos icones

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -12,20 +12,20 @@ function Rodape() {
           className=" link-icone"
           title="linkedin"
         >
-          <BsLinkedin size="10" color="#fff" />
+          <BsLinkedin size="20" color="#fff" />
         </a>
         <a
           href="https://github.com/jfilinho"
           className=" link-icone"
           title="github"
         >
-          <AiFillGithub size="10" color="#fff" />
+          <AiFillGithub size="20" color="#fff" />
         </a>
 
         <a href="https://www.instagram.com/filho_souzaa"
          className=" link-icone
         " title="instagram">
-          <FaInstagram size="10" color="#fff" />
+          <FaInstagram size="20" color="#fff" />
         </a>
 
         <a
@@ -33,7 +33,7 @@ function Rodape() {
           className=" link-icone"
           title="Facebook"
         >
-          <FaFacebook size="10" color="#fff" />
+          <FaFacebook size="20" color="#fff" />
         </a>
       </div>
       <p className="footer__copyright">
