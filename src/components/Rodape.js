25512f9f6a14914d6a4ed773da567527d9e4b19d
jfilinho <filinhosouza10@gmail.com>
2022-03-26 00:01:37 -0300

    padding

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -9,28 +9,28 @@ function Rodape() {
       <p className="footer_social">
         <a
           href="https://www.linkedin.com/in/jo%C3%A3o-filho-souza-wd-fullstack/"
-          className=" link-icone"
+          className=" link-icone mr-2"
           title="linkedin"
         >
           <BsLinkedin size="20" color="#fff" />
         </a>
         <a
           href="https://github.com/jfilinho"
-          className=" link-icone"
+          className=" link-icone mr-2"
           title="github"
         >
           <AiFillGithub size="20" color="#fff" />
         </a>
 
         <a href="https://www.instagram.com/filho_souzaa"
-         className=" link-icone
-        " title="instagram">
+         className=" link-icone mr-2"
+          title="instagram">
           <FaInstagram size="20" color="#fff" />
         </a>
 
         <a
           href="https://www.facebook.com/jfilhossouza"
-          className=" link-icone"
+          className=" link-icone mr-2"
           title="Facebook"
         >
           <FaFacebook size="20" color="#fff" />
