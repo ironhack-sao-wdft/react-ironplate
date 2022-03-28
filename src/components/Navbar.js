fc2c7701855a76a274ac722ba1e53b56f8334695
jfilinho <filinhosouza10@gmail.com>
2022-03-28 16:49:54 -0300

    resposividade naavbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,53 +9,56 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container  d-flex justify-content-between align-items-center text-light">
-            <button className="navbar-toggler" type="button" 
-              data-toggle="collapse" 
-              data-bs-target="#collapseExample"
-              aria-controls="navbarTogglerDemo01" 
-              aria-expanded="false" 
-              aria-label="Toggle navigation">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a className="navbar-brand" href="/">
+            Sistema
+          </a>
+
+          <div className="collapse navbar-collapse" id="#menuPrincipal">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  className={({ isActive }) =>
+                    `nav-link ${isActive ? "active" : ""}`
+                  }
+                  to="/"
+                >
+                  Home
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink
+                  className={({ isActive }) =>
+                    `nav-link ${isActive ? "active" : ""}`
+                  }
+                  to="/lista"
+                >
+                  Livros
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={({ isActive }) =>
+                    `nav-link ${isActive ? "active" : ""}`
+                  }
+                  to="/cadastrarLivro"
+                >
+                  Criar Livro
+                </NavLink>
+              </li>
+            </ul>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#collapseExample"
+              aria-controls="collapseExample"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon"></span>
-              </button>
-           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-              <a className="navbar-brand" href="/">Sistema</a>
-          
-
-
-        <div className="collapse navbar-collapse" id="#menuPrincipal">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? "active" : ""}`
-                }
-                to="/"
-              >
-                Home
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-              <NavLink
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? "active" : ""}`
-                }
-                to="/lista"
-              >
-                Livros
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? "active" : ""}`
-                }
-                to="/cadastrarLivro"
-              >
-                Criar Livro
-              </NavLink>
-            </li>
-          </ul>
+            </button>
           </div>
         </div>
         <div>
