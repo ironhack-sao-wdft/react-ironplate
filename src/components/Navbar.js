f3f486cc55939094bd295bfb541058a011179137
jfilinho <filinhosouza10@gmail.com>
2022-03-28 16:40:51 -0300

    mudando navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,15 +10,15 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container  d-flex justify-content-between align-items-center text-light">
             <button className="navbar-toggler" type="button" 
-              data-bs-toggle="collapse" 
-              data-bs-target="#navbarTogglerDemo01"
+              data-toggle="collapse" 
+              data-bs-target="#collapseExample"
               aria-controls="navbarTogglerDemo01" 
               aria-expanded="false" 
               aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
               </button>
            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-              <a className="navbar-brand" href="/">Hidden brand</a>
+              <a className="navbar-brand" href="/">Sistema</a>
           
 
 
