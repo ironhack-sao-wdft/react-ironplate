cd7404745456d9f2aceb31efeadf7c10afada770
jfilinho <filinhosouza10@gmail.com>
2022-03-28 15:50:07 -0300

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,86 +1,76 @@
 import { useContext } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-
+import { NavLink } from "react-router-dom";
 
 import { AuthContext } from "../contexts/authContext";
 
-function Navegador() {
+function Navbar() {
   const { loggedInUser, logout } = useContext(AuthContext);
 
   return (
-    <Navbar collapseOnSelect expand="lg" className="navegador">
-      <div className="container-fluid">
-        <Navbar.Brand href="/">
-          <img
-            src="./img/logo.png"
-            alt="logo"
-            width="140"
-            height="100"
-            className="imagem-logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container  d-flex justify-content-between align-items-center text-light">
+        <NavLink className="navbar-brand" to="/">
+          System Books
+        </NavLink>
 
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
-            <Nav.Link
-              className={({ isActive }) =>
-                `nav-Nav.Link ${isActive ? "active" : ""}`
-              }
-              href="/"
-              id="cor-home"
-            >
-              Home
-            </Nav.Link>
+        <div className="collapse navbar-collapse" id="#menuPrincipal">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? "active" : ""}`
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
 
-            <Nav.Link
-              className={({ isActive }) =>
-                `nav-Nav.Link ${isActive ? "active" : ""}`
-              }
-              href="/lista"
-              id="cor-list"
-            >
-              Livros
-            </Nav.Link>
-
-            <Nav.Link
-              className={({ isActive }) =>
-                `nav-Nav.Link ${isActive ? "active" : ""}`
-              }
-              href="/cadastrarLivro"
-              id="cor-criar"
-            >
-              Criar Livro
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? "active" : ""}`
+                }
+                to="/lista"
+              >
+                Livros
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  `nav-link ${isActive ? "active" : ""}`
+                }
+                to="/cadastrarLivro"
+              >
+                Criar Livro
+              </NavLink>
+            </li>
+          </ul>
+        </div>
         <div>
           {loggedInUser.user._id ? (
             <>
-              <span className="mr-3 text-light">
-                Bem-vindo, {loggedInUser.user.name}
-              </span>
+              <span className="mr-3">Bem-vindo, {loggedInUser.user.name}</span>
 
-              <button onClick={logout} className="btn btn-Nav.Link text-danger">
+              <button onClick={logout} className="btn btn-link">
                 Sair
               </button>
             </>
           ) : (
-            <Nav.Link
+            <NavLink
               className={({ isActive }) =>
-                `nav-Nav.Link ${isActive ? "active" : ""}`
+                `nav-link ${isActive ? "active" : ""}`
               }
-              href="/login"
+              to="/login"
             >
               Entrar
-            </Nav.Link>
+            </NavLink>
           )}
         </div>
       </div>
-    </Navbar>
+    </nav>
   );
 }
 
-export default Navegador;
+export default Navbar;
