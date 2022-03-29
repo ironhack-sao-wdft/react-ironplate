8b1aaf03ccd456a5a8c257a00f5afaae1ab5dd90
jfilinho <filinhosouza10@gmail.com>
2022-03-29 09:22:16 -0300

    referencias de links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -192,16 +192,15 @@ import Navbar from "react-bootstrap/Navbar";
 
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">System-Books</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            
-               
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/lista">Livros</Nav.Link>
+            <Nav.Link href="/cadastrarLivro">Novo Livro</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
