f0c1bccbf447cdde72698b46d9f65d676c30325c
jfilinho <filinhosouza10@gmail.com>
2022-03-30 20:00:04 -0300

    url herokuap

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apis = {
-  development: "http://localhost:4000/api",
-  production: "https://cadastro-livro-app.herokuapp.com/api",
+  development: "http://localhost:4000",
+  production: "https://cadastro-livro-app.herokuapp.com",
 };
 
 // Pré-configurando a URL padrão do nosso backend em uma instância do Axios
